1f5655a5ecf66b21be256fa06208d0c5f7ed9631
Ivan Padalko <dev@impadalko.com>
2023-04-23 17:22:12 -0300

    Add Uint8Array util to help decoding server response

---
+++ src/uint8ArrayUtil.ts
@@ -0,0 +1,67 @@
+class Uint8ArrayUtil {
+  /**
+   * Merge an array of Uint8Array into a single Uint8Array
+   * @param {Uint8Array[]} Array of Uint8Array to be merged
+   * @returns {Uint8Array} Merged Uint8Array
+   */
+  merge(arrays: Uint8Array[]): Uint8Array {
+    const length = arrays.reduce((acc, array) => acc + array.length, 0);
+
+    const mergedArray = new Uint8Array(length);
+    arrays.reduce((offset, array) => {
+      mergedArray.set(array, offset);
+      return offset + array.length;
+    }, 0);
+
+    return mergedArray;
+  }
+
+  /**
+   * Split an Uint8Array into an array of Uint8Array given a separator
+   * @param {Uint8Array} value to be split
+   * @param {Uint8Array} value to use as separator
+   * @returns {Uint8Array[]} Array of splitted tokens
+   */
+  split(data: Uint8Array, separator: Uint8Array): Uint8Array[] {
+    if (separator.length === 0) {
+      return [data];
+    }
+
+    const tokens: Uint8Array[] = [];
+    let start = 0;
+    for (let i = 0; i < data.length - separator.length + 1; i++) {
+      let match = true;
+      for (let j = 0; j < separator.length && match; j++) {
+        if (data[i + j] !== separator[j]) {
+          match = false;
+        }
+      }
+      if (match) {
+        tokens.push(data.slice(start, i));
+        start = i + separator.length;
+      }
+    }
+    if (start !== data.length) {
+      tokens.push(data.slice(start));
+    }
+    return tokens;
+  }
+
+  /**
+   * Trim zeroes at the end of an Uint8Array
+   * @param {Uint8Array} Uint8Array to be trimmed
+   * @returns {Uint8Array} Trimmed Uint8Array
+   */
+  trim(data: Uint8Array): Uint8Array {
+    let i = data.length - 1;
+    while (i >= 0) {
+      if (data[i] !== 0) {
+        break;
+      }
+      i--;
+    }
+    return data.slice(0, i + 1);
+  }
+}
+
+export default Uint8ArrayUtil;
---
+++ test/uint8ArrayUtil.test.ts
@@ -0,0 +1,82 @@
+import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
+import { describe, it } from "https://deno.land/std@0.182.0/testing/bdd.ts";
+
+import Uint8ArrayUtil from "src/uint8ArrayUtil.ts";
+
+describe("Uint8Array Util", () => {
+  const uint8ArrayUtil = new Uint8ArrayUtil();
+
+  it("should merge an empty array as an empty Uint8Array", () => {
+    const merged = uint8ArrayUtil.merge([]);
+
+    assertEquals(merged, new Uint8Array([]));
+  });
+
+  it("should merge an array with one element as an Uint8Array with this element", () => {
+    const merged = uint8ArrayUtil.merge([new Uint8Array([1, 2, 3])]);
+
+    assertEquals(merged, new Uint8Array([1, 2, 3]));
+  });
+
+  it("should merge an array with two elements as an Uint8Array with the merge of the elements", () => {
+    const merged = uint8ArrayUtil.merge([
+      new Uint8Array([1, 2, 3]),
+      new Uint8Array([4, 5]),
+    ]);
+
+    assertEquals(merged, new Uint8Array([1, 2, 3, 4, 5]));
+  });
+
+  it("should split an empty Uint8Array into an empty array", () => {
+    const split = uint8ArrayUtil.split(new Uint8Array([]), new Uint8Array([1]));
+
+    assertEquals(split, []);
+  });
+
+  it("should not split an Uint8Array when the separator is empty", () => {
+    const split = uint8ArrayUtil.split(
+      new Uint8Array([1, 2, 3]),
+      new Uint8Array([]),
+    );
+
+    assertEquals(split, [new Uint8Array([1, 2, 3])]);
+  });
+
+  it("should split an Uint8Array according to the separator", () => {
+    const split = uint8ArrayUtil.split(
+      new Uint8Array([1, 2, 4, 2, 3, 4, 5]),
+      new Uint8Array([2, 3]),
+    );
+
+    assertEquals(split, [new Uint8Array([1, 2, 4]), new Uint8Array([4, 5])]);
+  });
+
+  it("should split an Uint8Array properly when the separator is at the end", () => {
+    const split = uint8ArrayUtil.split(
+      new Uint8Array([1, 2, 4, 2, 3, 4, 5, 2, 3]),
+      new Uint8Array([2, 3]),
+    );
+
+    assertEquals(split, [new Uint8Array([1, 2, 4]), new Uint8Array([4, 5])]);
+  });
+
+  it("should not trim when the Uint8Array is empty", () => {
+    const trimmed = uint8ArrayUtil.trim(new Uint8Array([]));
+
+    assertEquals(trimmed, new Uint8Array([]));
+  });
+
+  it("should not trim when the Uint8Array does not have zeroes at the end", () => {
+    const trimmed = uint8ArrayUtil.trim(new Uint8Array([1, 1, 0, 1]));
+
+    assertEquals(trimmed, new Uint8Array([1, 1, 0, 1]));
+  });
+
+  it("should trim when the Uint8Array has zeroes at the end", () => {
+    const trimmed = uint8ArrayUtil.trim(
+      new Uint8Array([1, 1, 0, 1, 0, 0, 0, 0]),
+    );
+
+    assertEquals(trimmed, new Uint8Array([1, 1, 0, 1]));
+  });
+});
